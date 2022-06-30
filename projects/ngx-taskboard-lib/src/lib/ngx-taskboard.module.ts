0ec04d992db48282f266a3af77ed6c47e0aef6f2
Terrence Marion Castro <terrence.castro@gmail.com>
2022-06-30 15:37:41 +0800

    Replaced BrowserModule with CommonModule

--- projects/ngx-taskboard-lib/src/lib/ngx-taskboard.module.ts
+++ projects/ngx-taskboard-lib/src/lib/ngx-taskboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/platform-browser';
 import { BoardComponent } from './board/board.component';
 
 // Fontawesome
