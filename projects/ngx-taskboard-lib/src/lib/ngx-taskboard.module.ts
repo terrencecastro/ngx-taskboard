e0a469d018a127bd582817f7f236f4a5f65dacd2
Terrence Marion Castro <terrence.castro@gmail.com>
2022-06-30 19:11:11 +0800

    Update ngx-taskboard.module.ts

--- projects/ngx-taskboard-lib/src/lib/ngx-taskboard.module.ts
+++ projects/ngx-taskboard-lib/src/lib/ngx-taskboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BoardComponent } from './board/board.component';
 
 // Fontawesome
